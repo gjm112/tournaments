a6817d0e9d93bcc72042a0681145be65c19bdd16
zculp6 <zachculp6@gmail.com>
2025-11-07 16:33:10 -0600

    Updates

--- NFl2010theta.R
+++ NFl2010theta.R
@@ -19,4 +19,39 @@ theta <- data.frame(
 theta$theta[length(theta$theta)] <- 0
 theta %>% arrange(-theta)
 
+nfc_theta <- theta %>% filter(team %in%
+  c("Atlanta Falcons", "Chicago Bears", "Philadelphia Eagles", "Seattle Seahawks", "New Orleans Saints", "Green Bay Packers")) %>%
+  pull(theta)
+
+afc_theta <- theta %>% filter(team %in%
+  c("New England Patriots", "Pittsburgh Steelers", "Indianapolis Colts", "Kansas City Chiefs", "Baltimore Ravens", "New York Jets")) %>%
+  pull(theta)
+
+desired_nfc_order <- c(
+  "Atlanta Falcons",
+  "Chicago Bears",
+  "Philadelphia Eagles",
+  "Seattle Seahawks",
+  "New Orleans Saints",
+  "Green Bay Packers"
+)
+
+nfc_teams <- theta %>%
+  filter(team %in% desired_nfc_order) %>%
+  slice(match(desired_nfc_order, team)) %>%
+  pull(team)
+
+desired_afc_order <- c(
+  "New England Patriots",
+  "Pittsburgh Steelers",
+  "Indianapolis Colts",
+  "Kansas City Chiefs",
+  "Baltimore Ravens",
+  "New York Jets"
+)
+
+afc_teams <- theta %>%
+  filter(team %in% desired_afc_order) %>%
+  slice(match(desired_afc_order, team)) %>%
+  pull(team)
 
