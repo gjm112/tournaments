1290a3b102eeb882639b9defc667b2ad5a7c1232
Gregoery J. Matthews <gregorymatthews@Gregorys-MacBook-Pro.local>
2025-11-07 11:47:05 -0600

    updated.

---
+++ brackets_pictures.R
@@ -0,0 +1,51 @@
+plot(0,0,xlim = c(0,3), ylim = c(0,10), frame.plot= FALSE, xlab= "", ylab = "",xaxt= 'n', yaxt = 'n', col = "white", asp = 1/2)
+
+points(c(0,1), c(0,1), type = "l")
+points(c(0,1), c(2,1), type = "l")
+points(c(0,1), c(4,5), type = "l")
+points(c(0,1), c(6,5), type = "l")
+
+points(c(1,2), c(1,3), type = "l")
+points(c(1,2), c(5,3), type = "l")
+
+
+
+plot(0,0,xlim = c(0,3), ylim = c(0,30), frame.plot= FALSE, xlab= "", ylab = "",xaxt= 'n', yaxt = 'n', col = "white", asp = 1/3)
+
+points(c(0,1), c(0,2), type = "l")
+points(c(0,1), c(4,2), type = "l")
+points(c(0,1), c(8,10), type = "l")
+points(c(0,1), c(12,10), type = "l")
+points(c(0,1), c(16,18), type = "l")
+points(c(0,1), c(20,18), type = "l")
+points(c(0,1), c(24,26), type = "l")
+points(c(0,1), c(28,26), type = "l")
+
+points(c(1,2), c(2,6), type = "l")
+points(c(1,2), c(10,6), type = "l")
+points(c(1,2), c(26,22), type = "l")
+points(c(1,2), c(18,22), type = "l")
+
+points(c(2,3), c(22,14), type = "l")
+points(c(2,3), c(6,14), type = "l")
+
+#step ladder
+plot(0,0,xlim = c(0,3), ylim = c(0,10), frame.plot= FALSE, xlab= "", ylab = "",xaxt= 'n', yaxt = 'n', col = "white", asp = 1/2)
+
+points(c(0,-1), c(0,0), type = "l")
+points(c(0,-1), c(4,4), type = "l")
+points(c(1,0), c(6,6), type = "l")
+points(c(2,1), c(8,8), type = "l")
+
+points(c(0,1), c(0,2), type = "l")
+points(c(0,1), c(4,2), type = "l")
+
+points(c(1,2), c(2,4), type = "l")
+points(c(1,2), c(6,4), type = "l")
+
+points(c(2,3), c(8,6), type = "l")
+points(c(2,3), c(4,6), type = "l")
+
+
+
+
